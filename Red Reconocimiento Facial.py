873a9a9cc65d9af05926053d6966916b8c79dcd6
Jxel Rojas <jxelcut@gmail.com>
2023-11-19 19:04:26 -0600

    Nuevo modelo que entrena con los datos de imágenes de mi rostro, se corrió un primer experimento y los resultados se subieron a la carpeta "Resultados"
    -Jxel

--- Red Reconocimiento Facial.py
+++ Red Reconocimiento Facial.py
@@ -23,56 +23,7 @@ from keras.models import load_model, Model
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 np.set_printoptions(precision=4)
 
-"""
-#Eliminar el doble espacio entre algunos datos de la tabla
-with open('list_attr_celeba.txt', 'r') as f:
-   print("skipping : " + f.readline())
-   print("skipping headers : " + f.readline())
-   with open('attr_celeba_prepared.txt' , 'w') as newf:
-        for line in f:
-            new_line = ' '.join(line.split())
-            newf.write(new_line)
-            newf.write('\n')
 
-
-#Lee el archivo ya corregido y lo carga a un dataframe de pandas
-df = pd.read_csv("attr_celeba_prepared.txt" , sep=' ', header = None)
-
-
-#Crea conjuntos de datos para nombres de archivos y atributos 
-files = tf.data.Dataset.from_tensor_slices(df[0])
-
-attributes = tf.data.Dataset.from_tensor_slices(df.iloc[:,1:].to_numpy())
-
-
-#Combina los conjuntos de datos en uno solo
-data = tf.data.Dataset.zip((files, attributes))
-
-
-#Ruta del directorio donde están las imágenes
-path_to_images = 'img_align_celeba/'
-
-
-#Se procesan las imágenes con sus atributos
-def process_file(file_name, attributes):
-    image = tf.io.read_file(path_to_images + file_name)
-    image = tf.image.decode_jpeg(image, channels=3)
-    image = tf.image.resize(image, [192, 192])
-    image /= 255.0 
-    return image, attributes
-
-#Aplica la función de procesamiento a cada elemento del conjunto de datos
-labeled_images = data.map(process_file)
-
-#Visualiza las dos primeras imágenes del conjunto de datos.
-for image, attri in labeled_images.take(2):
-    plt.imshow(image)
-    plt.show()
-
-# Dividir los datos en conjuntos de entrenamiento y prueba
-train_df, test_df = train_test_split(df, test_size=0.2)
-
-"""
 # Cargar el modelo entrenado
 loaded_model = load_model('model.h5')
 
